180180f72ac1566ff9601467388db7063b34cffb
Howard <hskim0209@nate.com>
2018-06-25 04:54:38 +0000

    C9 업로드

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get '/' => 'home#index'# home 컨트롤러 內 index 액션(메소드)
   get '/home/lotto' # => 'home#lotto' (원래 루트와 같다면 생략 가능) # home 컨트롤러 內 lotto 액션
   get '/lunch' => 'home#lunch'
-  get '/serch' => 'home#serch'
+  get '/search' => 'home#search'
 end
